f1e4f4a3ed80a8f8a04bdd0b35becbcc188dcd58
zhulinghuanyu <421821939@qq.com>
2026-08-06 17:08:58 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -15,6 +15,15 @@ BEIJING_TZ = timezone(timedelta(hours=8))
 SUB_APIS = [
     "https://api.v1.mk/sub",
     "https://url.v1.mk/sub",
+    "https://sub.d1.mk/sub",
+    "https://sub.xeton.dev/sub",
+    "https://sub.xeton.de/sub",
+    "https://subapi.cmliussss.net/sub",
+    "https://subapi.fxxk.dedyn.io/sub",
+    "https://api.dler.io/sub",
+    "https://sub.dlsj.me/sub",
+    "https://api.wcc.best/sub",
+    "https://www.nameless13.com/sub",
 ]
 EXCLUDE_KEYWORDS = [
     "cloudflareinsights.com",
